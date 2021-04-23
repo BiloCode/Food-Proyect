cffcbb956eb0a4b1f44d66edd476729f67030ee7
josedaesbar <josedaesbar@gmail.com>
2021-04-23 14:10:15 -0500

    BranchOfficeList - Slider
    Slider con transicion y cambio de imagenes

--- website/src/components/molecules/SucursalSlider/index.tsx
+++ website/src/components/molecules/SucursalSlider/index.tsx
@@ -3,17 +3,37 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import * as S from "./styles";
 
 import Image01 from "assets/images/Hamburguesa.jpg";
+import Image02 from "assets/images/Hamburguesa_02.jpg";
 import Icon from "components/atoms/Icon";
+import { LegacyRef, useRef } from "react";
 
 const SucursalSlider = () => {
+  const images = [Image01, Image02];
+
+  const ImagesRef: LegacyRef<HTMLDivElement> = useRef(null);
+
+  const onClickPrevius = () => {
+    ImagesRef!.current.style.transform = "translateX(0)";
+  };
+
+  const onClickNext = () => {
+    ImagesRef!.current.style.transform = "translateX(-100%)";
+  };
+
   return (
     <S.SucursalSliderContainer>
-      <Image src={Image01} />
+      <S.SucursalSliderImages ref={ImagesRef}>
+        {images.map((v, i) => (
+          <S.Image key={i}>
+            <Image src={v} />
+          </S.Image>
+        ))}
+      </S.SucursalSliderImages>
       <S.SucursalSliderArrows>
-        <S.SucursalArrow>
+        <S.SucursalArrow onClick={onClickPrevius}>
           <Icon color="white" size="medium" Type={IoIosArrowBack} />
         </S.SucursalArrow>
-        <S.SucursalArrow>
+        <S.SucursalArrow onClick={onClickNext}>
           <Icon color="white" size="medium" Type={IoIosArrowForward} />
         </S.SucursalArrow>
       </S.SucursalSliderArrows>
--- website/src/components/molecules/SucursalSlider/styles.ts
+++ website/src/components/molecules/SucursalSlider/styles.ts
@@ -3,17 +3,29 @@ import styled from "styled-components";
 export const SucursalSliderContainer = styled.div`
   width: 100%;
   height: 370px;
-  object-fit: cover;
+  overflow: hidden;
   position: relative;
+`;
+
+export const SucursalSliderImages = styled.div`
+  width: 100%;
   display: flex;
-  align-items: center;
+  transition-duration: 1s;
+`;
+
+export const Image = styled.div`
+  width: 100%;
+  flex: none;
 `;
 
 export const SucursalSliderArrows = styled.div`
   position: absolute;
   width: 100%;
+  height: 100%;
   padding: 0 2em;
+  top: 0;
   display: flex;
+  align-items: center;
   justify-content: space-between;
   box-sizing: border-box;
 `;
