0134e0d214ff169ced0a96c6b11e975b49a5c5aa
BiloCode <billy_paredes_aycho@hotmail.com>
2021-05-08 11:54:41 -0500

    Arreglo de fuentes en la aplicacion * Las fuentes ahora se cargan en el html

--- website/src/styles.ts
+++ website/src/styles.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Open+Sans:wght@300;400;600;700;800&display=swap');
-
   :root {
     --font: 'Noto Sans', sans-serif;
     --extra-font: 'Open Sans', sans-serif;
